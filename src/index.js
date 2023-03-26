45f227da94b885a36f336cd343f41f948cd181ce
Professor77777 <shoxboz0696@gmail.com>
2023-03-26 15:00:40 +0900

    tasnk on process

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+const photos = [
+  { url: "", name: "Sardor" },
+  { url: "", name: "Orange" },
+];
+root.render(
+  <React.StrictMode>
+    {photos.map((value) => {
+      return <App data={value} />;
+    })}
+  </React.StrictMode>
+);
